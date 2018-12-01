e494f1faf2138d9501991c7cff06d6bf7d6721e9
tomoyukiHAYAKAWA <x14091xx@aitech.ac.jp>
2018-12-01 15:52:49 +0900

    first commit

---
+++ CheckDiary/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CheckDiary
+//
+//  Created by Tomoyuki Hayakawa on 2018/12/01.
+//  Copyright © 2018 Tomoyuki Hayakawa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
